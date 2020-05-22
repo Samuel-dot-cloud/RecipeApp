98a6c8dbb67390f0c521ace6592b756749c8e261
Samuel-dot-cloud <successam336@gmail.com>
2020-05-22 20:02:21 +0300

    Binding of views in recipe page and linking to about page

--- app/src/main/java/com/studiofive/recipeapp/AboutActivity.java
+++ app/src/main/java/com/studiofive/recipeapp/AboutActivity.java
@@ -2,6 +2,7 @@ package com.studiofive.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AboutActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        Intent intent = getIntent();
     }
 }
--- app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
+++ app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
@@ -4,15 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RecipeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findAboutButton)
+    Button mFindAboutButton;
+    @BindView(R.id.displaySearch)
+    TextView mDisplaySearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
+
+        mFindAboutButton.setOnClickListener(this);
 
 
         Intent intent = getIntent();
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mFindAboutButton){
+            Intent intent = new Intent(RecipeActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
     }
 }

4ca1a9c802effc22f45a4840b03ef4785ae9c20f
Samuel-dot-cloud <successam336@gmail.com>
2020-05-24 11:44:31 +0300

    Addition of list to recipe page

--- app/src/main/java/com/studiofive/recipeapp/MyRecipesArrayAdapter.java
+++ app/src/main/java/com/studiofive/recipeapp/MyRecipesArrayAdapter.java
@@ -1,4 +1,33 @@
 package com.studiofive.recipeapp;
 
-public class MyRecipesArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyRecipesArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mFoods;
+    private String[] mBase;
+
+    public MyRecipesArrayAdapter(Context mContext, int resource, String[] mFoods, String[] mBase){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mFoods = mFoods;
+        this.mBase = mBase;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String foods = mFoods[position];
+        String base = mBase[position];
+        return String.format("%s \nBase category: %s",foods,  base);
+
+    }
+
+    @Override
+    public int getCount(){
+        return mFoods.length;
+    }
 }
+
+
+
--- app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
+++ app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,8 +25,12 @@ public class RecipeActivity extends AppCompatActivity implements View.OnClickLis
     TextView mDisplaySearch;
 //    @BindView(R.id.menuBar)
 //    BottomNavigationView mMenuBar;
+    @BindView(R.id.listView)
+    ListView mListView;
 
-    private String[] foods = new String[] {"Flatbread", "Chips", "Fish", "Pork", "Coffee", "Rice", "Burgers", "Chicken", "Cake", "Hotdog", "BCuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+    private String[] foods = new String[] {"Flatbread", "Chips", "Fish", "Pork", "Coffee", "Rice", "Burgers", "Chicken", "Cake", "Hotdog", "Barbeque","Pizza", "Omelet", "Sausage", "Croissant", "Guacamole" };
+    private String[] base = new String[] {"Flour", "Plant", "Meat", "Meat", "Drink", "Plant", "Plant and meat", "Meat", "Flour", "Flour,plant and meat", "Meat","Flour, plant and meat", "Egg", "Meat","Flour", "Plant" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,19 @@ public class RecipeActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_recipe);
         ButterKnife.bind(this);
 
+
+        MyRecipesArrayAdapter adapter = new MyRecipesArrayAdapter(this, android.R.layout.simple_list_item_1, foods, base);
+        mListView.setAdapter(adapter);
+
+        //Setting a toaster to display recipe and base
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RecipeActivity.this, restaurant, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 //        mFindAboutButton.setOnClickListener(this);
 

508a3c9e4c19d5245b3582caf9ee5d8e7892f1e8
Samuel-dot-cloud <successam336@gmail.com>
2020-05-24 10:41:32 +0300

    switching to button in recipe activity

--- app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
+++ app/src/main/java/com/studiofive/recipeapp/RecipeActivity.java
@@ -15,13 +15,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecipeActivity extends AppCompatActivity {
-//    @BindView(R.id.findAboutButton)
-//    Button mFindAboutButton;
+public class RecipeActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findAboutButton)
+    Button mFindAboutButton;
     @BindView(R.id.displaySearch)
     TextView mDisplaySearch;
-    @BindView(R.id.menuBar)
-    BottomNavigationView mMenuBar;
+//    @BindView(R.id.menuBar)
+//    BottomNavigationView mMenuBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,41 +32,41 @@ public class RecipeActivity extends AppCompatActivity {
 //        mFindAboutButton.setOnClickListener(this);
 
 
-        //setting recipes selected with nav bar
-        mMenuBar.setSelectedItemId(R.id.recipes);
-
-        //Perform ItemSelectedListener
-        mMenuBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (mMenuBar.getSelectedItemId()){
-                    case R.id.nav_home:
-                        startActivity(new Intent(getApplicationContext()
-                                ,MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.recipes:
-                        return true;
-                    case R.id.about:
-                        startActivity(new Intent(getApplicationContext()
-                                ,AboutActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
-            }
-        });
+//        //setting recipes selected with nav bar
+//        mMenuBar.setSelectedItemId(R.id.recipes);
+//
+//        //Perform ItemSelectedListener
+//        mMenuBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (mMenuBar.getSelectedItemId()){
+//                    case R.id.nav_home:
+//                        startActivity(new Intent(getApplicationContext()
+//                                ,MainActivity.class));
+//                        overridePendingTransition(0,0);
+//                        return true;
+//                    case R.id.recipes:
+//                        return true;
+//                    case R.id.about:
+//                        startActivity(new Intent(getApplicationContext()
+//                                ,AboutActivity.class));
+//                        overridePendingTransition(0,0);
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
 
         Intent intent = getIntent();
         String recipe = intent.getStringExtra("recipe");
         mDisplaySearch.setText("Recipes found associated with search item:" + recipe);
     }
 
-//    @Override
-//    public void onClick(View v){
-//        if(v == mFindAboutButton){
-//            Intent intent = new Intent(RecipeActivity.this, AboutActivity.class);
-//            startActivity(intent);
-//        }
-//    }
+    @Override
+    public void onClick(View v){
+        if(v == mFindAboutButton){
+            Intent intent = new Intent(RecipeActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
+    }
 }

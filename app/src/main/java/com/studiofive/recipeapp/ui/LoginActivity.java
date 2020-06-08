67fe127963701c6962cfc0ca3b1f526a8c9acfc8
Samuel-dot-cloud <successam336@gmail.com>
2020-06-08 19:20:55 +0300

    caddition of logic to login form

---
+++ app/src/main/java/com/studiofive/recipeapp/ui/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.studiofive.recipeapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.studiofive.recipeapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+    }
+}
